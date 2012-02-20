1da3e29a639b0404484ed70c2b412075a7acc5cb
James Fuller <jim.fuller@marklogic.com>
2012-02-20 22:28:13 +0100

    added gen dmg utility script so I dont lose it

---
+++ bin/gendmg.sh
@@ -0,0 +1,1 @@
+/Applications/IzPack/utils/wrappers/izpack2app/izpack2app.py /Users/jfuller/Source/Webcomposite/xquerydoc/dist/xquerydoc-0.1.jar /Users/jfuller/Source/Webcomposite/xquerydoc/dist/xquerydoc-0.1.app
